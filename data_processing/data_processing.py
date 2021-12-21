661c4cece8e9725ea9edac7c58d162f19f3636fc
Akash Nair <akashnair@GS-5674.local>
2021-12-21 13:24:07 +0530

    parameter updated

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -1,9 +1,14 @@
-import outlier_detection as od
+import outlier_detection
+import edge_case_selection
 
 
 
-input_path = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/dogs-vs-cats/sample_data/"
-output_path = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/dogs-vs-cats/"
+input_path = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/dogs-vs-cats/sample_data/gygygs"
+output_path = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/dogs-vs-cats/vgvhbhb"
+outlier_data_percent = 10
+non_outlier_data_percent = 40
 
 
-od.outlierDetection(input_path,output_path)
+outlier_detection.outlierDetection(input_path,output_path)
+
+edge_case_selection.edgeCaseSelection(input_path,output_path, non_outlier_data_percent , outlier_data_percent)
