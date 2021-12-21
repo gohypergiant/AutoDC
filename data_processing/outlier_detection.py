816e16c1dee2b4e1484eb7c5e81cfcd137f2516d
Akash Nair <akashnair@GS-5674.local>
2021-12-21 13:25:12 +0530

    validation added

--- data_processing/outlier_detection.py
+++ data_processing/outlier_detection.py
@@ -15,7 +15,8 @@ import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 import cv2
-
+import logging
+logger = logging.getLogger('ftpuploader')
 import edge_case_selection
 
 img2vec = Img2Vec()
@@ -28,119 +29,127 @@ img2vec = Img2Vec()
 
 def outlierDetection(input_path,output_path):
 
-	list_of_dir = [ name for name in os.listdir(input_path) if os.path.isdir(os.path.join(input_path, name)) ]
-	image_classes = list_of_dir
+	try:
 
-	for image_class in image_classes:
-	    image_paths = []
-	    image_paths.extend(glob.glob(input_path + image_class + '/*.jpg'))
 
-	    image_vectors = {}
-	    for image_path in image_paths:
-	        vector = img2vec.get_vec(image_path)
-	        image_vectors[image_path] = vector
 
-	    X = np.stack(list(image_vectors.values()))
+		list_of_dir = [ name for name in os.listdir(input_path) if os.path.isdir(os.path.join(input_path, name)) ]
+		image_classes = list_of_dir
 
-	    pca_50 = PCA(n_components=50)
-	    pca_result_50 = pca_50.fit_transform(X)
-	    print('Cumulative explained variation for 50 principal components: {}'.format(np.sum(pca_50.explained_variance_ratio_)))
-	    print(np.shape(pca_result_50))
+		for image_class in image_classes:
+		    image_paths = []
+		    image_paths.extend(glob.glob(input_path + image_class + '/*.jpg'))
+
+		    image_vectors = {}
+		    for image_path in image_paths:
+		        vector = img2vec.get_vec(image_path)
+		        image_vectors[image_path] = vector
+
+		    X = np.stack(list(image_vectors.values()))
+
+		    pca_50 = PCA(n_components=50)
+		    pca_result_50 = pca_50.fit_transform(X)
+		    print('Cumulative explained variation for 50 principal components: {}'.format(np.sum(pca_50.explained_variance_ratio_)))
+		    print(np.shape(pca_result_50))
+		    
+		    tsne = TSNE(n_components=2, verbose=1, n_iter=3000)
+		    tsne_result = tsne.fit_transform(pca_result_50)
+
+		    tsne_result_scaled = StandardScaler().fit_transform(tsne_result)
+		    # plt.scatter(tsne_result_scaled[:,0], tsne_result_scaled[:,1])
+
+		    # images = []
+		    # for image_path in image_paths:
+		    #     image = cv2.imread(image_path, 3)
+		    #     b,g,r = cv2.split(image)           # get b, g, r
+		    #     image = cv2.merge([r,g,b])         # switch it to r, g, b
+		    #     image = cv2.resize(image, (50,50))
+		    #     images.append(image)    
+
+
+		    # fig, ax = plt.subplots(figsize=(20,15))
+		    # artists = []
+
+		    # for xy, i in zip(tsne_result_scaled, images):
+		    #     x0, y0 = xy
+		    #     img = OffsetImage(i, zoom=.7)
+		    #     ab = AnnotationBbox(img, (x0, y0), xycoords='data', frameon=False)
+		    #     artists.append(ax.add_artist(ab))
+		    # ax.update_datalim(tsne_result_scaled)
+		    # ax.autoscale(enable=True, axis='both', tight=True)
+		    # plt.show()
+
+
+
+		    # Outlier detection
+		    from sklearn.ensemble import IsolationForest
+
+		    clf = IsolationForest(random_state=123)
+		    preds = clf.fit_predict(tsne_result_scaled)
+
+		    import shutil
+
+		    images = []
+		    image_paths_outlier = []
+		    count = 0
+		    outlier_count = 0
+		    non_outlier_count = 0
+
+		    if not os.path.exists(output_path+"output/outliers/outlier_data/"+image_class):
+		    	os.makedirs(output_path+"output/outliers/outlier_data/"+image_class)
+		    # else:
+		    # 	all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
+		    # 	for f in all_files:
+		    # 		os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
+
+
+		    if not os.path.exists(output_path+"output/outliers/non_outlier_data/"+image_class):
+		    	os.makedirs(output_path+"output/outliers/non_outlier_data/"+image_class)
+		    # else:
+		    # 	all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
+		    # 	for f in all_files:
+		    # 		os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
+
+
+		    tsne_result_scaled_outlier = []
+		    for image_path in image_paths:
+		        if preds[count] == -1:
+		            image_paths_outlier.append(image_path)
+		 
+		            image = cv2.imread(image_path, 3)
+		            b,g,r = cv2.split(image)           # get b, g, r
+		            image = cv2.merge([r,g,b])         # switch it to r, g, b
+		            image = cv2.resize(image, (50,50))
+		            images.append(image)
+		            tsne_result_scaled_outlier.append(tsne_result_scaled[count])
+		            outlier_count = outlier_count + 1
+
+		            # Remove all existing files
+		            # all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
+		            # for f in all_files:
+		            # 	os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
+
+		            imgName =  os.path.split(image_path)[1]
+		            shutil.copy(image_path, output_path+"output/outliers/outlier_data/"+image_class+"/"+imgName)
+		        else:
+
+		            # all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
+		            # for f in all_files:
+		            # 	os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
+
+		            imgName =  os.path.split(image_path)[1]
+		            shutil.copy(image_path, output_path+"output/outliers/non_outlier_data/"+image_class+"/"+imgName)
+		            non_outlier_count = non_outlier_count+1
+		        count = count + 1
+		        
+		    # print("outlier_count : ",outlier_count)
+		    # print("non_outlier_count : ",non_outlier_count)
+
+		return True
+	except Exception as e:
+		logger.error('Something went wrong: ' + str(e))
 	    
-	    tsne = TSNE(n_components=2, verbose=1, n_iter=3000)
-	    tsne_result = tsne.fit_transform(pca_result_50)
-
-	    tsne_result_scaled = StandardScaler().fit_transform(tsne_result)
-	    # plt.scatter(tsne_result_scaled[:,0], tsne_result_scaled[:,1])
-
-	    # images = []
-	    # for image_path in image_paths:
-	    #     image = cv2.imread(image_path, 3)
-	    #     b,g,r = cv2.split(image)           # get b, g, r
-	    #     image = cv2.merge([r,g,b])         # switch it to r, g, b
-	    #     image = cv2.resize(image, (50,50))
-	    #     images.append(image)    
-
-
-	    # fig, ax = plt.subplots(figsize=(20,15))
-	    # artists = []
-
-	    # for xy, i in zip(tsne_result_scaled, images):
-	    #     x0, y0 = xy
-	    #     img = OffsetImage(i, zoom=.7)
-	    #     ab = AnnotationBbox(img, (x0, y0), xycoords='data', frameon=False)
-	    #     artists.append(ax.add_artist(ab))
-	    # ax.update_datalim(tsne_result_scaled)
-	    # ax.autoscale(enable=True, axis='both', tight=True)
-	    # plt.show()
-
-
-
-	    # Outlier detection
-	    from sklearn.ensemble import IsolationForest
-
-	    clf = IsolationForest(random_state=123)
-	    preds = clf.fit_predict(tsne_result_scaled)
-
-	    import shutil
-
-	    images = []
-	    image_paths_outlier = []
-	    count = 0
-	    outlier_count = 0
-	    non_outlier_count = 0
-
-	    if not os.path.exists(output_path+"output/outliers/outlier_data/"+image_class):
-	    	os.makedirs(output_path+"output/outliers/outlier_data/"+image_class)
-	    # else:
-	    # 	all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
-	    # 	for f in all_files:
-	    # 		os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
-
-
-	    if not os.path.exists(output_path+"output/outliers/non_outlier_data/"+image_class):
-	    	os.makedirs(output_path+"output/outliers/non_outlier_data/"+image_class)
-	    # else:
-	    # 	all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
-	    # 	for f in all_files:
-	    # 		os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
-
-
-	    tsne_result_scaled_outlier = []
-	    for image_path in image_paths:
-	        if preds[count] == -1:
-	            image_paths_outlier.append(image_path)
-	 
-	            image = cv2.imread(image_path, 3)
-	            b,g,r = cv2.split(image)           # get b, g, r
-	            image = cv2.merge([r,g,b])         # switch it to r, g, b
-	            image = cv2.resize(image, (50,50))
-	            images.append(image)
-	            tsne_result_scaled_outlier.append(tsne_result_scaled[count])
-	            outlier_count = outlier_count + 1
-
-	            # Remove all existing files
-	            # all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
-	            # for f in all_files:
-	            # 	os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
-
-	            imgName =  os.path.split(image_path)[1]
-	            shutil.copy(image_path, output_path+"output/outliers/outlier_data/"+image_class+"/"+imgName)
-	        else:
-
-	            # all_files = os.listdir(output_path+"output/outliers/outlier_data/"+image_class)
-	            # for f in all_files:
-	            # 	os.remove(output_path+"output/outliers/outlier_data/"+image_class+"/"+f)
-
-	            imgName =  os.path.split(image_path)[1]
-	            shutil.copy(image_path, output_path+"output/outliers/non_outlier_data/"+image_class+"/"+imgName)
-	            non_outlier_count = non_outlier_count+1
-	        count = count + 1
-	        
-	    print("outlier_count : ",outlier_count)
-	    print("non_outlier_count : ",non_outlier_count)
-	    
-	edge_case_selection.edgeCaseSelection(input_path,output_path, 40 , 10)
+	# edge_case_selection.edgeCaseSelection(input_path,output_path, 40 , 10)
 
 
 # outlierDetection(input_path,output_path)
