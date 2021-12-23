61553560dcf0240dab48862ed762338b34eb2975
Akash Nair <akashnair@GS-5674.local>
2021-12-24 02:21:19 +0530

    data augmentation module added

--- data_processing/data_augmentation.py
+++ data_processing/data_augmentation.py
@@ -1,60 +1,71 @@
-# # pip install Pillow if you don't already have it
-
-# # import image utilities
-# from PIL import Image
-
-# # import os utilities
-# import os
-
-# # imagePath = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/Roman_numbers_dataset/data/outlier_data_augment/train/ix/"
-# # imagePath = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/dogs-vs-cats/outlier_data_augment/training/dogs/"
-# imagePath = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/snails_dataset/outlier_data_augment/training_set/Melanoides/"
-
-# # define a function that rotates images in the current directory
-# # given the rotation in degrees as a parameter
-# def rotateImages(rotationAmt):
-#   # for each image in the current directory
-# #   for image in os.listdir(os.getcwd()):
-#   for image in os.listdir(imagePath):  
-#     print("image path : ", image)
-#     # open the image
-#     img = Image.open(imagePath+image)
-#     # rotate and save the image with the same filename
-#     img.rotate(rotationAmt).save(imagePath+"aug_"+image)
-#     # close the image
-#     img.close()
-    
-# # examples of use
-# rotateImages(180)
+import os
+import shutil, random, os
+import logging
+logger = logging.getLogger('ftpuploader')
+import augly.image as imaugs
 
 
 
+def imageAugmentation(input_path,output_path, aug_data_percent, aug_technique):
+
+    try:
+
+        outlier_dir = output_path+"output/outliers/outlier_data/"
+
+        list_of_dir_outlier = [ name for name in os.listdir(outlier_dir) if os.path.isdir(os.path.join(outlier_dir, name)) ]
+        # list_of_dir_non_outlier = [ name for name in os.listdir(non_outlier_dir) if os.path.isdir(os.path.join(non_outlier_dir, name)) ]
+        
+        image_classes = list_of_dir_outlier
+
+        for image_class in image_classes:
+
+            image_class_outlier_dir = outlier_dir+image_class
+
+            total_outlier_data = len(os.listdir(image_class_outlier_dir))
+            
+            total_outlier_data_to_select = int((int(aug_data_percent)/100)*total_outlier_data)
+
+            # print("class : ", image_class)
+            # print("outlier data : ", total_outlier_data_to_select)
+
+            improved_output_dir = output_path+"output/improved_data/"+image_class
+
+            # Create dir for improve data if not exist
+            if not os.path.exists(improved_output_dir):
+                os.makedirs(improved_output_dir)
 
 
-import cv2 as cv
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-#instantiate the ImageDataGenerator class
-datagen = ImageDataGenerator(
-        rotation_range=40,
-        height_shift_range=0.2,
-        width_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest')
-#loop over the data in batches and this automatically saves the images
+            # Copy files from outlier folder to final data folder
+            # Non outlier data specific to an image class eg. "cats"
+            # Outlier data 
+            dirpath_outlier = outlier_dir+image_class
+            filenames_outlier = random.sample(os.listdir(dirpath_outlier), total_outlier_data_to_select)
+            for fname in filenames_outlier:
+
+                if(aug_technique == "noise"):
+                    aug_image = imaugs.random_noise(outlier_dir+image_class+"/"+fname, output_path=output_path+"output/improved_data/"+image_class+"/aug_"+fname)
+                elif(aug_technique == "crop"):
+                    aug_image = imaugs.crop(outlier_dir+image_class+"/"+fname, output_path=output_path+"output/improved_data/"+image_class+"/aug_"+fname)
+                elif(aug_technique == "vflip"):
+                    aug_image = imaugs.vflip(outlier_dir+image_class+"/"+fname, output_path=output_path+"output/improved_data/"+image_class+"/aug_"+fname)
+                elif(aug_technique == "rotate"):
+                    aug_image = imaugs.rotate(outlier_dir+image_class+"/"+fname, output_path=output_path+"output/improved_data/"+image_class+"/aug_"+fname)
+                elif(aug_technique == "saturation"):
+                    aug_image = imaugs.saturation(outlier_dir+image_class+"/"+fname, output_path=output_path+"output/improved_data/"+image_class+"/aug_"+fname)
+                elif(aug_technique == "brightness"):
+                    aug_image = imaugs.brightness(outlier_dir+image_class+"/"+fname, output_path=output_path+"output/improved_data/"+image_class+"/aug_"+fname)
+                elif(aug_technique == "scale"):
+                    aug_image = imaugs.scale(outlier_dir+image_class+"/"+fname, output_path=output_path+"output/improved_data/"+image_class+"/aug_"+fname)
+                else:
+                    logger.error('Something went wrong: Invalid Augmentation Technique')
+                    return False
+
+        return True
+    except Exception as e:
+        logger.error('Something went wrong: ' + str(e))
 
 
 
-input_path = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/dogs-vs-cats/sample_data/"
-output_path = "/Users/akashnair/Desktop/Akash/Project/Hypergiant/Data_Labeling/dogs-vs-cats/"
 
-outlier_dir = output_path+"output/outliers/outlier_data"
-aug_outlier_dir = output_path+"output/aug"
 
-i = 0
-for batch in datagen.flow_from_directory('outlier_dir', batch_size=6,target_size=(256,256),
-                          save_to_dir='path_to_output_folder', save_format='jpg'):
-    i += 1
-    if i > 5:
-        break
+
