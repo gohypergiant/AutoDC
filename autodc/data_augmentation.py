9a6067aa910127d029458072da47b05816d22ac4
Sylvia Tran <sylvia.tran@hypergiant.com>
2022-03-16 09:24:17 -0700

    data augmentation refactor, still req nb testing

--- autodc/data_augmentation.py
+++ autodc/data_augmentation.py
@@ -3,92 +3,105 @@ import random
 import os
 
 import augly.image as imaugs
+from PIL import Image
 
 
 logger = logging.getLogger('ftpuploader')
 
 
+# @ZAC, please fill in the doc string for `aug_data_ratio`
 class DataAugmentation:
-    def __init__(self, input_path, output_path, aug_data_ratio, aug_technique):
+    def __init__(self, input_path: str, output_path: str,
+                 aug_data_ratio: float, aug_technique: str,
+                 verbose: bool = False):
+        """
+        :param input_path: input file directory where images are located
+        :param output_path: output file directory location to write outlier image data
+        :param aug_data_ratio:
+        :param aug_technique: user-specified autmentation technique
+        """
         self.input_path, = input_path
-        self.output_path = output_path
+        self.output_path = output_path.rstrip('/')
         self.aug_data_ratio = aug_data_ratio
         self.aug_technique = aug_technique
 
-    def image_data(
-        input_path: str,
-        output_path: str,
-        aug_data_ratio: int,
-        aug_technique: str
-        ):
+        self.verbose = verbose
 
-        def image_aug_technique(
-            aug_technique,
-            outlier_path_string,
-            output_path_string
-            ):
+        self.outlier_dir = None
+        self.image_classes = None
 
-            aug_functions = dict(
-                noise=imaugs.random_noise(outlier_path_string, output_path_string),
-                crop=imaugs.crop(outlier_path_string, output_path_string),
-                vflip=imaugs.vflip(outlier_path_string, output_path_string),
-                rotate=imaugs.rotate(outlier_path_string, output_path_string),
-                saturation=imaugs.saturation(outlier_path_string, output_path_string),
-                brightness=imaugs.brightness(outlier_path_string, output_path_string),
-                scale=imaugs.scale(outlier_path_string, output_path_string)
-            )
 
-            if aug_technique.lower() not in aug_functions:
-                raise ValueError(f'Invalid Augmentation Technique: {list(aug_functions.keys())}')
+    def get_image_classes(self):
+        """Get image classes to use
+        :return: list of image classes
+        """
+        self.outlier_dir = f"{self.output_path}/output/outliers/outlier_data/"
+        list_of_dir_outlier = [name for name in os.listdir(self.outlier_dir)
+                               if os.path.isdir(os.path.join(self.outlier_dir, name))]
+        if self.verbose:
+            print("AUTODC: Collected image classes --------\n")
 
-            return aug_functions.get(aug_technique)
+        self.image_classes = list_of_dir_outlier
+        return self.image_classes
 
-        try:
+    @staticmethod
+    def image_aug_technique(aug_technique: str, outlier_path: str, output_path: str) -> Image:
+        """Augments image located in outlier_path based on specified aug_technique, and writes to the specified output path
+        :param aug_technique: augmentation technique as a string
+        :param outlier_path: outlier image filepath
+        :param output_path: image filepath to write augmented image to
+        :return: an augmented PIL Image
+        """
+        aug_functions = dict(
+            noise=imaugs.random_noise(outlier_path, output_path),
+            crop=imaugs.crop(outlier_path, output_path),
+            vflip=imaugs.vflip(outlier_path, output_path),
+            rotate=imaugs.rotate(outlier_path, output_path),
+            saturation=imaugs.saturation(outlier_path, output_path),
+            brightness=imaugs.brightness(outlier_path, output_path),
+            scale=imaugs.scale(outlier_path, output_path)
+        )
 
-            outlier_dir = output_path+"output/outliers/outlier_data/"
-            list_of_dir_outlier = [ name for name in os.listdir(outlier_dir) if os.path.isdir(os.path.join(outlier_dir, name)) ]
-            # list_of_dir_non_outlier = [ name for name in os.listdir(non_outlier_dir) if os.path.isdir(os.path.join(non_outlier_dir, name)) ]
+        if aug_technique.lower() not in aug_functions:
+            raise ValueError(f'Invalid Augmentation Technique: {list(aug_functions.keys())}')
+        return aug_functions.get(aug_technique)
 
-            print(" ")
-            print("### AUTODC: Data Augmentation -- In Progress --------")
-            print(" ")
+    # @ZAC - please complete the doc string for what this method returns / does
+    def augment_image_data(self) -> bool:
+        """
+        :return:
+        """
+        for image_class in self.image_classes:
+            image_class_outlier_dir = f"{self.outlier_dir}{image_class}"
+            total_outlier_data = len(os.listdir(image_class_outlier_dir))
 
-            image_classes = list_of_dir_outlier
+            total_outlier_data_to_select = int((int(self.aug_data_ratio) / 100) * total_outlier_data)
 
-            for image_class in image_classes:
+            if self.verbose:
+                print(f"class: {image_class}")
+                print(f"outlier data: {total_outlier_data_to_select}")
 
-                image_class_outlier_dir = outlier_dir+image_class
+            improved_output_dir = f"{self.output_path}/output/improved_data/{image_class}"
+            if not os.path.exists(improved_output_dir):
+                os.makedirs(improved_output_dir)
 
-                total_outlier_data = len(os.listdir(image_class_outlier_dir))
+            # Copy files from outlier folder to final data folder
+            # Non outlier data specific to an image class eg. "cats"
 
-                total_outlier_data_to_select = int((int(aug_data_ratio)/100)*total_outlier_data)
+            # Outlier data
+            dirpath_outlier = f"{self.outlier_dir}{image_class}"
+            filenames_outlier = random.sample(os.listdir(dirpath_outlier), total_outlier_data_to_select)
+            output_path_string = f"{self.output_path}/output/improved_data/{image_class}/aug_"
+            for fname in filenames_outlier:
+                print(fname)
+                DataAugmentation.image_aug_technique(self.aug_technique,
+                                                     f"{dirpath_outlier}/{fname}",
+                                                     f"{output_path_string}{fname}")
+        if self.verbose:
+            print("\n### AUTODC: Data Augmentation -- Completed --------\n")
 
-                # print("class : ", image_class)
-                # print("outlier data : ", total_outlier_data_to_select)
+        return True
 
-                improved_output_dir = output_path+"output/improved_data/"+image_class
 
-                # Create dir for improve data if not exist
-                if not os.path.exists(improved_output_dir):
-                    os.makedirs(improved_output_dir)
-
-                # Copy files from outlier folder to final data folder
-                # Non outlier data specific to an image class eg. "cats"
-
-                # Outlier data
-                dirpath_outlier = outlier_dir+image_class
-                filenames_outlier = random.sample(os.listdir(dirpath_outlier), total_outlier_data_to_select)
-                outlier_path_string = outlier_dir+image_class+"/"
-                output_path_string =output_path+"output/improved_data/"+image_class+"/aug_"
-                for fname in filenames_outlier:
-                    print(fname)
-                    image_aug_technique(aug_technique, outlier_path_string+fname, output_path_string+fname)
-
-            print(" ")
-            print("### AUTODC: Data Augmentation -- Completed --------")
-            print(" ")
-
-            return True
-
-        except Exception as e:
-            logger.error('Something went wrong: ' + str(e))#, exc_info=sys.exc_info())
+if __name__ == "__main__":
+    None
